e6a14fc93c43807c3b82d62d8a3691b75dc7b170
captiainR <rajath.v@techmilltechnologies.com>
2021-03-19 20:42:46 +0530

    adding changes from asus

--- app/src/main/java/com/lavie/motorcycle_HUD_Instructables/MainActivity.java
+++ app/src/main/java/com/lavie/motorcycle_HUD_Instructables/MainActivity.java
@@ -1,24 +1,33 @@
 package com.lavie.motorcycle_HUD_Instructables;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Color;
 import android.location.Location;
 import android.os.BatteryManager;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.gson.JsonObject;
 import com.mapbox.android.core.permissions.PermissionsListener;
 import com.mapbox.android.core.permissions.PermissionsManager;
 import com.mapbox.api.directions.v5.DirectionsCriteria;
 import com.mapbox.api.directions.v5.models.DirectionsResponse;
 import com.mapbox.api.directions.v5.models.DirectionsRoute;
+import com.mapbox.api.geocoding.v5.models.CarmenFeature;
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.FeatureCollection;
 import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.location.LocationComponent;
 import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions;
@@ -28,7 +37,10 @@ import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
-
+import com.mapbox.mapboxsdk.plugins.places.autocomplete.PlaceAutocomplete;
+import com.mapbox.mapboxsdk.plugins.places.autocomplete.model.PlaceOptions;
+import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
 import com.mapbox.services.android.navigation.ui.v5.NavigationView;
 import com.mapbox.services.android.navigation.ui.v5.NavigationViewOptions;
 import com.mapbox.services.android.navigation.ui.v5.OnNavigationReadyCallback;
@@ -41,9 +53,7 @@ import com.mapbox.services.android.navigation.v5.routeprogress.ProgressChangeLis
 import com.mapbox.services.android.navigation.v5.routeprogress.RouteProgress;
 
 import java.io.OutputStream;
-
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
@@ -56,6 +66,9 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconImage;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconOffset;
+
 public class MainActivity extends AppCompatActivity implements PermissionsListener, OnNavigationReadyCallback,
         OffRouteListener, ProgressChangeListener, NavigationListener, OnMapReadyCallback, MapboxMap.OnMapClickListener {
 
@@ -65,6 +78,11 @@ public class MainActivity extends AppCompatActivity implements PermissionsListen
     private Button closeBTButton;
 
     //  maps related
+    private static final int REQUEST_CODE_AUTOCOMPLETE = 1;
+    private String geojsonSourceLayerId = "geojsonSourceLayerId";
+    private String symbolIconId = "symbolIconId";
+    private CarmenFeature home;
+    private CarmenFeature work;
     private PermissionsManager permissionsManager;
     private NavigationView navigationView;
     private MapboxMap mapboxMap;
@@ -96,25 +114,26 @@ public class MainActivity extends AppCompatActivity implements PermissionsListen
         mapView = (MapView) findViewById(R.id.mapView);
         startButton = findViewById(R.id.buttonStart);
         startButton.setEnabled(false);  // Cannot launch navigation before route has been fetched
-       /* connectBTButton = findViewById(R.id.buttonConnectBT);
+        connectBTButton = findViewById(R.id.buttonConnectBT);
         connectBTButton.setEnabled(false); // Cannot attempt to connect before checking if bluetooth is on
         closeBTButton = findViewById(R.id.buttonCloseBT);
         closeBTButton.setEnabled(false);   // Cannot attempt to close bluetooth connection if it hasn't been established yet
-
-        // Setup serial bluetooth management
-        if (bluetoothManagement.setupBT_noErrors()){
-            connectBTButton.setEnabled(true);
-        }
-
-        // Setup test buttons
-        connectBTButton.setOnClickListener(v -> {
-            if (bluetoothManagement.connectBT_noErrors()) {
-                closeBTButton.setEnabled(true);
-            }
-        });
-        closeBTButton.setOnClickListener(v -> {
-            bluetoothManagement.closeBT();   //Close all Bluetooth devices
-        });*/
+connectBTButton.setVisibility(View.GONE);
+        closeBTButton.setVisibility(View.GONE);
+//        // Setup serial bluetooth management
+//        if (bluetoothManagement.setupBT_noErrors()){
+//            connectBTButton.setEnabled(true);
+//        }
+//
+//        // Setup test buttons
+//        connectBTButton.setOnClickListener(v -> {
+//            if (bluetoothManagement.connectBT_noErrors()) {
+//                closeBTButton.setEnabled(true);
+//            }
+//        });
+//        closeBTButton.setOnClickListener(v -> {
+//            bluetoothManagement.closeBT();   //Close all Bluetooth devices
+//        });
 
         startButton.setOnClickListener(v -> {
             setTheme(R.style.Theme_AppCompat_Light_NoActionBar);
@@ -131,13 +150,13 @@ public class MainActivity extends AppCompatActivity implements PermissionsListen
         RequestBody formBody = new FormBody.Builder()
                 .add("direction", direction)
                 .add("distance", distance)
-                .add("time",time)
+               .add("time",time)
                 .add("temperature", temp)
                 .build();
 
 try {
     Request request = new Request.Builder()
-            .url("192.168.4.1") // The URL to send the data to
+            .url("http://192.168.43.26/") // The URL to send the data to
             .post(formBody)
             .build();
 }
@@ -157,8 +176,16 @@ catch (Throwable t){
                 new Style.OnStyleLoaded() {
                     @Override
                     public void onStyleLoaded(@NonNull Style style) {
-                        mapboxMap.addOnMapClickListener(MainActivity.this);
-                        enableLocationComponent(style);
+                        initSearchFab();
+
+// Create an empty GeoJSON source using the empty feature collection
+                        setUpSource(style);
+
+// Set up a new symbol layer for displaying the searched location's feature coordinates
+                        setupLayer(style);
+
+//                        mapboxMap.addOnMapClickListener(MainActivity.this);
+//                        enableLocationComponent(style);
                     }
                 });
     }
@@ -169,6 +196,86 @@ catch (Throwable t){
         DESTINATION = Point.fromLngLat(point.getLongitude(), point.getLatitude());
         fetchRoute();
         return true;
+    }
+
+    private void initSearchFab() {
+        findViewById(R.id.fab_location_search).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new PlaceAutocomplete.IntentBuilder()
+                        .accessToken(Mapbox.getAccessToken() != null ? Mapbox.getAccessToken() : getString(R.string.access_token))
+                        .placeOptions(PlaceOptions.builder()
+                                .backgroundColor(Color.parseColor("#EEEEEE"))
+                                .limit(10)
+//                               .addInjectedFeature(home)
+//                               .addInjectedFeature(work)
+                                .build(PlaceOptions.MODE_CARDS))
+                        .build(MainActivity.this);
+                startActivityForResult(intent, REQUEST_CODE_AUTOCOMPLETE);
+            }
+        });
+    }
+
+/*    private void addUserLocations() {
+        home = CarmenFeature.builder().text("Mapbox SF Office")
+                .geometry(Point.fromLngLat(-122.3964485, 37.7912561))
+                .placeName("50 Beale St, San Francisco, CA")
+                .id("mapbox-sf")
+                .properties(new JsonObject())
+                .build();
+
+        work = CarmenFeature.builder().text("Mapbox DC Office")
+                .placeName("740 15th Street NW, Washington DC")
+                .geometry(Point.fromLngLat(-77.0338348, 38.899750))
+                .id("mapbox-dc")
+                .properties(new JsonObject())
+                .build();
+    }*/
+
+    private void setUpSource(@NonNull Style loadedMapStyle) {
+        loadedMapStyle.addSource(new GeoJsonSource(geojsonSourceLayerId));
+    }
+
+    private void setupLayer(@NonNull Style loadedMapStyle) {
+        loadedMapStyle.addLayer(new SymbolLayer("SYMBOL_LAYER_ID", geojsonSourceLayerId).withProperties(
+                iconImage(symbolIconId),
+                iconOffset(new Float[] {0f, -8f})
+        ));
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE_AUTOCOMPLETE) {
+
+// Retrieve selected location's CarmenFeature
+            CarmenFeature selectedCarmenFeature = PlaceAutocomplete.getPlace(data);
+
+// Create a new FeatureCollection and add a new Feature to it using selectedCarmenFeature above.
+// Then retrieve and update the source designated for showing a selected location's symbol layer icon
+
+            if (mapboxMap != null) {
+                Style style = mapboxMap.getStyle();
+                if (style != null) {
+                    GeoJsonSource source = style.getSourceAs(geojsonSourceLayerId);
+                    if (source != null) {
+                        source.setGeoJson(FeatureCollection.fromFeatures(
+                                new Feature[] {Feature.fromJson(selectedCarmenFeature.toJson())}));
+                    }
+
+// Move map camera to the selected location
+                    mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(
+                            new CameraPosition.Builder()
+                                    .target(new LatLng(((Point) selectedCarmenFeature.geometry()).latitude(),
+                                            ((Point) selectedCarmenFeature.geometry()).longitude()))
+                                    .zoom(14)
+                                    .build()), 4000);
+                }
+                DESTINATION = Point.fromLngLat(((Point) selectedCarmenFeature.geometry()).latitude(),
+                        ((Point) selectedCarmenFeature.geometry()).longitude());
+
+                fetchRoute();
+            }
+        }
     }
 
     private void fetchRoute() {
@@ -276,6 +383,8 @@ catch (Throwable t){
             permissionsManager.requestLocationPermissions(this);
         }
     }
+
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
